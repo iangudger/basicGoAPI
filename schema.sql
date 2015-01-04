8579ad536c7ac11b3a3ee8ba808dde568c601d4f
Ian <git@loosescre.ws>
2015-01-04 02:00:17 -0800

    Added default value for date.

--- schema.sql
+++ schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE users (
    id               SERIAL                   PRIMARY KEY,
    email            TEXT           NOT NULL  UNIQUE,
    password         TEXT           NOT NULL,
-   date             TIMESTAMP      NOT NULL
+   date             TIMESTAMP      NOT NULL  DEFAULT now()
 );
 
 CREATE TABLE sessions (
